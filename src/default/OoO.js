925716ad2dd917d2cab8762b486898e3f3a71b8b
Glitch (noon-revolver) <none>
2018-05-24 07:17:03 +0000

    🤗🚿 Checkpoint

---
+++ src/default/OoO.js
@@ -0,0 +1,9 @@
+window .Oo = function () {
+	var val = arguments [0]
+
+	for (i = 1; i < arguments .length; i ++) {
+		;val = args [i] (val) }
+
+	return val }
+window .o = x => x
+window .oO = x => x
