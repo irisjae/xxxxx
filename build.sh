7d8350702cb2f14034d13e936debb3a88ca5816a
Glitch (noon-revolver) <none>
2018-05-31 04:10:50 +0000

    💓🐇 Checkpoint ./build.sh:1186802/124

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 rm -r ~/static/*
 cp -r ~/src/* ~/static/
 
-find ~/static/ -type f | grep default | while read f; do 
+find ~/static/ -type f | grep default\\. | while read f; do 
+  mv "$f" "$(dirname $(dirname $f))/$(echo $(basename $f) | sed s/default/$(basename $(dirname $f))/)"
+done
+
