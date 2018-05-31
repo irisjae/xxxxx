9923fd8d2ea944b8ab358aef99eb82d703d40dd0
Glitch (noon-revolver) <none>
2018-05-31 04:21:20 +0000

    🐧🔥 Checkpoint ./build.sh:1186802/286

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ find ~/static/ -type f | grep default\\. | while read f; do
   mv "$f" "$(dirname $(dirname $f))/$(echo $(basename $f) | sed s/default/$(basename $(dirname $f))/)"
 done
 
+find ~/static/ -type f | grep \\.js | while read f; do
+  node -e "var fs = require ('fs'); var compiler = require ('surplus/compiler'); compiler .compile (fs .readFileSync ('$f'), { sourcemap: 'append' });" | tee "$f"
+done
