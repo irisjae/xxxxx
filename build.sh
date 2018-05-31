6adfbd6ff9a82e945617eb7a1b325cb9ea4cd5cc
Glitch (noon-revolver) <none>
2018-05-31 07:20:17 +0000

    🐇👬 Checkpoint ./build.sh:1186802/238 ./src/default/default.js:1186802/621 ./static/teacher.js:1186802/10

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 find ~/static -type f | grep -v default\\.js | xargs -I {} rm {}
 find ~/src -maxdepth 1 | grep -v default | xargs -I {} cp -r {} ~/static
 
-find ~/static/ -type f | grep -v ^default\\.js | grep default\\. | while read f; do 
+find ~/static/ -type f ! -path ~/static/default.js | grep default | while read f; do 
   mv "$f" "$(dirname $(dirname $f))/$(echo $(basename $f) | sed s/default/$(basename $(dirname $f))/)"
 done
 
-find ~/static/ -type f | grep -v default/ | grep \\.js | while read f; do
+find ~/static/ -type f ! -path ~/static/default.js | grep \\.js | while read f; do
   echo "$(node -e "var fs = require ('fs'); var compiler = require ('surplus/compiler'); console .log (compiler .compile (fs .readFileSync ('$f') .toString (), { sourcemap: 'append' }));")" > "$f"
 done
 
