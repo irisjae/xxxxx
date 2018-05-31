e3f179ced9f5e9a11bb0b3bb60e607ad7b208f84
Glitch (noon-revolver) <none>
2018-05-31 05:12:16 +0000

    💐📮 Checkpoint ./build.sh:1186802/1178

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 rm -r ~/static/*
 cp -r ~/src/* ~/static/
 
-find ~/static/ -type f | grep default\\. | while read f; do 
+find ~/static/ -type f | grep -v default/ | grep default\\. | while read f; do 
   mv "$f" "$(dirname $(dirname $f))/$(echo $(basename $f) | sed s/default/$(basename $(dirname $f))/)"
 done
 
-find ~/static/ -type f | grep \\.js | while read f; do
+find ~/static/ -type f | grep -v default/ | grep \\.js | while read f; do
 #  node -e "var fs = require ('fs'); console .log (fs .readFileSync ('$(realpath --relative-to="./" $f)') .toString ());"
-  node -e "var fs = require ('fs'); var compiler = require ('surplus/compiler'); console .log (/*compiler .compile (*/fs .readFileSync ('$(realpath --relative-to="./" $f)') .toString ()/*, { sourcemap: 'append' })*/);" | tee "$f"
+  echo "$(node -e "var fs = require ('fs'); var compiler = require ('surplus/compiler'); console .log (compiler .compile (fs .readFileSync ('$(realpath --relative-to="./" $f)') .toString (), { sourcemap: 'append' }));")" > "$f"
 done
+
+touch ~/static/default.js
+find ~/static/ -type f | grep default/ | grep \\.js | grep -v default\\.js | while read f; do
+  cat "$f" >> ~/static/default.js
+  rm "$f"
+done
+cat ~/static/default/default.js >> ~/static/default.js
+rm ~/static/default/default.js
