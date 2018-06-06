317d0c1a7ea5803dda77a7061321d803f2937714
Glitch (noon-revolver) <none>
2018-06-06 04:23:41 +0000

    🦀💿 Checkpoint ./src/default/default.js:1186802/2869 ./src/default/aux.js:1186802/3092

--- src/default/aux.js
+++ src/default/aux.js
@@ -1,4 +1,10 @@
-window .shuffle = list => {
+var { xx, oo, Oo, L, R, S, Z, 
+  do_, defined, data,
+  fro, every
+} = window .stuff
+
+
+var shuffle = list => {
   var array = []
   for (var i in list) {
     ;array .push (list [i])}
@@ -6,11 +12,112 @@ window .shuffle = list => {
     var j = Math .floor (Math .random () * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]] // eslint-disable-line no-param-reassign
   }
-  return array
-}
-window .post = x => ({
+  return array }
+
+
+
+
+
+var api = (room, x) => fetch ('/log/' + room, x) .then (x => x .json ())
+var post = x => ({
   method: 'POST',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json' },
   body: JSON .stringify (x) })
+
+
+
+
+//--------------------TYPES--------------------
+
+var number = defined
+var string = defined
+var list = a => defined
+var maybe = a => defined
+var id = window .string
+
+var student = id
+var question = string
+var answer = question
+var latency = number
+var v = (...types) => defined
+
+
+var attempt = data ({ attempt: (guess = answer, time = latency) => defined })
+var performance = data ({ performance: (attempts = list (attempt)) => defined })
+var history = data ({ history: (performances = list (performance)) => defined })
+
+var rules = data ({ rules: (time_limit = number, size = number) => defined })
+
+var setup = data ({ setup: ( room = room, questions = list (question), rules = rules ) => defined })
+
+
+var teacher_app = data ({
+	ready: ( setup = setup ) => defined,
+	during: ( setup = setup, completed_questions = history ) => defined,
+	done: ( setup = setup, completed_questions = history ) => defined })
+var student_app = data ({
+	ready: ( setup = maybe (setup) ) => defined,
+	during: ( setup = setup, completed_questions = history ) => defined,
+	done: ( setup = setup, completed_questions = history ) => defined })
+var io = data ({
+  inert: () => defined,
+  connecting: () => defined })
+
+
+var message = data ({
+  setup: ( questions = list (question), rules = rules ) => defined })
+var consensus = data ({
+  consensus: ( students = list (v (student, latency, history)), latency ) => defined })
+
+
+
+
+
+var default_questions = shuffle ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+var default_rules = rules .rules (10, 10)
+
+
+
+
+var as_maybe = [L .reread (x => Z .Just (x)), L .defaults (Z .Nothing)]
+
+var state_room = [ state_setup, setup_room ]
+var as_maybe = [L .reread (x => Z .Just (x)), L .defaults (Z .Nothing)]
+
+var state_ready = ['ready']
+var state_during = ['during']
+var state_done = ['done']
+var state_setup = [L .choices (state_ready, state_during), 'setup']
+var state_students = [L .choices (state_ready, state_during), 'students']
+var setup_room = ['setup', 'room']
+var setup_questions = ['setup', 'questions']
+var setup_rules = ['setup', 'rules']
+
+var io_inert = ['inert']
+var io_connecting = ['connecting']
+
+var state_room = [ state_setup, setup_room ]
+
+
+var consensus_questions = ['setup', 'questions'] 
+
+
+
+
+
+
+var log_consensus = msgs =>
+  R .reduce (R .mergeDeepRight, {}, msgs)
+
+
+
+
+
+
+
+window .stuff = { ...window .stuff,
+  number, string, list, maybe, id,
+  shuffle,
+  api, post }
--- src/default/default.js
+++ src/default/default.js
@@ -63,7 +63,6 @@ var every = x => {
 
 
 
-var api = (room, x) => fetch ('/log/' + room, x) .then (x => x .json ())
 
 
 
@@ -85,101 +84,8 @@ document .addEventListener ('DOMContentLoaded', _ => {;
 
 
 
-//--------------------TYPES--------------------
-
-var number = defined
-var string = defined
-var list = a => defined
-var maybe = a => defined
-var id = window .string
-
-var student = id
-var question = string
-var answer = question
-var latency = number
-var v = (...types) => defined
-
-
-var attempt = data ({ attempt: (guess = answer, time = latency) => defined })
-var performance = data ({ performance: (attempts = list (attempt)) => defined })
-var history = data ({ history: (performances = list (performance)) => defined })
-
-var rules = data ({ rules: (time_limit = number, size = number) => defined })
-
-var setup = data ({ setup: ( room = room, questions = list (question), rules = rules ) => defined })
-
-
-var teacher_app = data ({
-	ready: ( setup = setup ) => defined,
-	during: ( setup = setup, completed_questions = history ) => defined,
-	done: ( setup = setup, completed_questions = history ) => defined })
-var student_app = data ({
-	ready: ( setup = maybe (setup) ) => defined,
-	during: ( setup = setup, completed_questions = progress ) => defined,
-	done: () => defined })
-var io = data ({
-  inert: () => defined,
-  connecting: () => defined })
-
-
-var message = data ({
-  setup: ( questions = list (question), rules = rules ) => defined })
-var consensus = data ({
-  consensus: ( students = list (v (student, latency, history)), latency ) => defined })
-
-
-
-
-
-var default_questions = shuffle ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-var default_rules = rules .rules (10, 10)
-
-
-var as_maybe = [L .reread (x => Z .Just (x)), L .defaults (Z .Nothing)]
-
-
-var state_setup = [L .choices ('ready', 'during'), 'setup']
-var setup_room = ['setup', 'room']
-var setup_questions = ['setup', 'questions']
-var setup_rules = ['setup', 'rules']
-var state_students = [L .choices ('ready', 'during'), 'students']
-
-var state_room = [ state_setup, setup_room ]
-var as_maybe = [L .reread (x => Z .Just (x)), L .defaults (Z .Nothing)]
-
-var state_ready = ['ready']
-var state_during = ['during']
-var state_done = ['done']
-var state_setup = [L .choices (state_ready, state_during), 'setup']
-var setup_room = ['setup', 'room']
-var setup_questions = ['setup', 'questions']
-var setup_rules = ['setup', 'rules']
-
-var io_inert = ['inert']
-var io_connecting = ['connecting']
-
-var state_room = [ state_setup, setup_room ]
-
-
-var consensus_questions = ['setup', 'questions'] 
-
-
-
-
-
-
-var log_consensus = msgs =>
-  R .reduce (R .mergeDeepRight, {}, msgs)
-
-
-
-
-
-
-
 window .Surplus = Surplus
 window .stuff = { ...window .stuff,
   xx, oo, Oo, L, R, S, Z, 
   do_, defined, data,
-  number, string, list, maybe, id,
-  fro, api, every }
+  fro, every }
