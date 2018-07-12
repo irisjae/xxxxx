6c6f9d87ff72411a56c70b6cdd9940ace9c43fcb
Glitch (noon-revolver) <none>
2018-07-12 10:15:56 +0000

    🚙🐦 Checkpoint ./reimport.sh:1186802/1242 ./bundle.sh:1186802/8

--- reimport.sh
+++ reimport.sh
@@ -1,1 +1,2 @@
-find ~/src/ -type f | grep default/ | grep \\.js | 
+imports="var $(find ~/src/ -type f | grep default/ | grep \\.js | xargs -I {} sed '1,/^[[:space:]]*window \.stuff.*$/d' {} | sed 's/ }/,\n/' | sed 's/^[[:space:]]*//' | sed -z 's/\(.*\),/{ \1 }/') = window .stuff"
+
