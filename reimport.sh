931237ad8000425c9446afc379692582a05d30d5
Glitch (noon-revolver) <none>
2018-07-12 11:45:43 +0000

    😿💘 Checkpoint ./reimport.sh:1186802/95

--- reimport.sh
+++ reimport.sh
@@ -1,3 +1,3 @@
 imports="var $(find ~/src/ -type f | grep default/ | grep \\.js | xargs -I {} sed '1,/^[[:space:]]*window \.stuff.*$/d' {} | sed 's/ }/,\n/' | sed 's/^[[:space:]]*//' | sed -z 's/\(.*\),/{ \1\n}/') = window .stuff"
 
-find ~/src -mindepth 1 -maxdepth 1 | grep -v default | xargs -I {} find {} -mindepth 1 | grep default\.js | xargs -I {} echo "cat << $(printf "'EOF'\n")$imports$(printf "\nEOF")$(printf "\n;")"'echo "$(sed '\''1,/^} = window \.stuff$/d'\'' {})"' 
+find ~/src -mindepth 1 -maxdepth 1 | grep -v default | xargs -I {} find {} -mindepth 1 | grep default\.js | xargs -I {} bash -c "cat << $(echo -e "'EOF'\n$imports\nEOF\n(")"'echo "$(sed '\''1,/^} = window \.stuff$/d'\'' "{}")")' 
