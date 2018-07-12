7a6688bd5ed9e0d272cf4e7980aceb447c3b8a01
Glitch (noon-revolver) <none>
2018-07-12 11:24:56 +0000

    📇🏰 Checkpoint ./reimport.sh:1186802/389

--- reimport.sh
+++ reimport.sh
@@ -1,3 +1,3 @@
 imports="var $(find ~/src/ -type f | grep default/ | grep \\.js | xargs -I {} sed '1,/^[[:space:]]*window \.stuff.*$/d' {} | sed 's/ }/,\n/' | sed 's/^[[:space:]]*//' | sed -z 's/\(.*\),/{ \1\n}/') = window .stuff"
 
-find ~/src -mindepth 1 -maxdepth 1 | grep -v default | xargs -I {} find {} -mindepth 1 | grep default\.js | xargs -I {} echo "$imports$(sed '1,/^} = window \.stuff$/d' {})" 
+find ~/src -mindepth 1 -maxdepth 1 | grep -v default | xargs -I {} find {} -mindepth 1 | grep default\.js | xargs -I {} echo "cat << 'EOF'$(echo)$imports$(echo)EOF$(echo; echo)"'echo "$(sed '\''1,/^} = window \.stuff$/d'\'' {})"' 
