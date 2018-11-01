fb2f1678b6c2aa193f5d8f0b32c3b27e282a66a8
Glitch (noon-revolver) <none>
2018-11-01 05:35:59 +0000

    🐿😂 Checkpoint ./src/default/default.js:1186802/12

--- src/default/default.js
+++ src/default/default.js
@@ -24,7 +24,7 @@ var apply = fn => arg_list =>
 
 
 
-var as_sole = [ L .elems, L .reread (list => !! (list .length === 1) ? list [0] : undefined) ]
+var as_sole = [ L .reread (list => !! (list .length === 1) ? list [0] : undefined) ]
 var sole = list =>
   !! (list .length === 1) ? list [0]
   : panic (list + ' is not sole')
