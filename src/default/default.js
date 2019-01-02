62d928d76e0bbc19997704f6683f4bdf8057c9fa
Glitch (noon-revolver) <none>
2019-01-02 07:53:55 +0000

    🔔🐊 Checkpoint ./src/default/default.js:1186802/179

--- src/default/default.js
+++ src/default/default.js
@@ -361,7 +361,9 @@ document .addEventListener ('DOMContentLoaded', _ => {;
   //var morphdom = require ('morph-dom')
   //var nanomorph = require ('nanomorph')
   var morph = goals => {;
-    ;dd .apply (view, dd .diff (view, goals))
+    ;dd .apply (view
+      , dd .diff (view, goals)
+        .filter (R .complement (under ('name') (Z_ .equals ('secret')))) )
     ;[] .forEach .call (document .querySelectorAll ('input'), _input => {;_input .dispatchEvent (new Event ('input', { bubbles: true }))})  } 
     //;morphdom (view, goals) } 
     //;nanomorph (view, goals) }
