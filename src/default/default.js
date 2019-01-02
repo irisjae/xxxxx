4b1b369c7438a096f834b12fe448cdf87925e243
Glitch (noon-revolver) <none>
2019-01-02 05:57:56 +0000

    💑🌒 Checkpoint ./src/default/default.js:1186802/29

--- src/default/default.js
+++ src/default/default.js
@@ -362,7 +362,7 @@ document .addEventListener ('DOMContentLoaded', _ => {;
   //var nanomorph = require ('nanomorph')
   var morph = goals => {;
     ;dd .apply (view, dd .diff (view, goals))
-    ;[] .forEach .call (document .querySelectorAll ('input') .dispatchEvent (new Event ('input', { bubbles: true })))  } 
+    ;[] .forEach .call (document .querySelectorAll ('input'), _input => {;_input .dispatchEvent (new Event ('input', { bubbles: true }))})  } 
     //;morphdom (view, goals) } 
     //;nanomorph (view, goals) }
   ;morph (window .view), (new MutationObserver (mutations => {;
