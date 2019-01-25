a4d52ff1a1b3589758c4bf21285941dc77ed96d7
Glitch (noon-revolver) <none>
2019-01-25 09:37:54 +0000

    😜🚂 Checkpoint ./scripts/bundle.sh:1186802/49 ./src/default/aux.js:1186802/2

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -36,5 +36,5 @@ find ~/static/ -type d -empty -delete
 
 find ~/static/ -type f | grep \\.js | while read f; do
   echo babeling...
-  pnpx -p babel-cli -p babel-preset-env -p @babel/plugin-proposal-object-rest-spread babel "$f" --presets=env --plugins @babel/plugin-proposal-object-rest-spread > ~/temp
+  pnpx -p babel-cli -p @babel/preset-env -p @babel/plugin-proposal-object-rest-spread babel "$f" --presets=@babel/preset-env --plugins @babel/plugin-proposal-object-rest-spread > ~/temp
   mv ~/temp "$f" ;done
