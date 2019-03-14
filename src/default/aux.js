9120ddc4a9f5534d0f2e05e7f0b250516015f352
Glitch (noon-revolver) <none>
2019-03-14 10:10:17 +0000

    ☂️🍴 Checkpoint ./src/default/aux.js:1186802/1473

--- src/default/aux.js
+++ src/default/aux.js
@@ -428,14 +428,15 @@ var current_problem = by (_app =>
 var attempted_positions = by (_past =>
   L .collect ([ past_as_points, L .elems, point_as_position ]))
 
-// make more elegant
 var as_solved_on = memoize (_board =>
-  L .when (memoize (_point => so ((_=_=>
-    _position && problem_choice_matches (_problem) (_choice),
-    where
-    , _problem = T (_point) (L .get (point_as_problem))
-    , _position = T (_point) (L .get (point_as_position))
-    , _choice = _position && T (_board) (L .get ([ as_position (_position), cell_as_choice ])) )=>_))) )
+  L .choose (_point =>
+    L .chain (_position =>
+      L .when (so ((_=_=>
+        problem_choice_matches (_problem) (_choice),
+        where
+        , _problem = T (_point) (L .get (point_as_problem))
+        , _choice = T (_board) (L .get ([ as_position (_position), cell_as_choice ])) )=>_))
+    ) (point_as_position)) )
 
 var solved_positions = _board => by (_past => 
   L .collect ([ past_as_points, L .elems, as_solved_on (_board), point_as_position ]))
@@ -446,6 +447,7 @@ var bingoed_positions = _board => _past =>
 // make more elegant
 var bingoes = _board => _past => 
   so ((_=_=>
+  result
   T (_solved_positions
   ) (
   [ $ (Z_ .reduce
