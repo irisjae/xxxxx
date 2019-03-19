a90241f3ff9579fa7bada6085826f724bc4f809d
Jae <irrisjay@gmail.com>
2019-03-19 15:54:26 +0800

    ?

--- src/teacher/default.js
+++ src/teacher/default.js
@@ -193,7 +193,7 @@ var app_boards_state = belief (app_as_boards) (app_state)
 var app_pasts_state = belief (app_as_pasts) (app_state)
 
 var app_current_problem_state = belief (current_problem) (app_state)
-var app_students_map_boards_v_pasts_state = belief ([ L .pick ({ boards: app_boards_state, pasts: app_pasts_state }), ({ boards, pasts }) => map_zip (a => b => [a, b]) (boards) (pasts) ]) (app_state)
+var app_students_map_boards_v_pasts_state = belief ([ L .pick ({ boards: app_boards_state, pasts: app_pasts_state }), L .when (I), ({ boards, pasts }) => map_zip (a => b => [a, b]) (boards) (pasts) ]) (app_state)
 
 var lookbehind_nothing_state = belief (lookbehind_as_nothing) (lookbehind_state)
 var lookbehind_preview_questions_state = belief (lookbehind_as_preview_questions) (lookbehind_state)
