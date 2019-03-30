180f9a095cb0407b354c7d0f94ad64c5162d9d07
Jae <irrisjay@gmail.com>
2019-03-30 16:23:44 +0800

    ?

--- src/default/default.js
+++ src/default/default.js
@@ -29,7 +29,6 @@ var so = fn_form =>
 	!! (fn_form .toString () .endsWith ('=>_'))
 	? fn_form () ()
 	: fn_form ()
-//var and_by = $ ([ by, Z .flip ])
 var go = Promise .resolve ()
 var never = new Promise (_ => {})
 
@@ -55,87 +54,80 @@ var __data_lens = new Map
 var fiat = {}
 var data = cons_definitions =>
 	T (cons_definitions
-	) (L .modify (L .values) ((def_fn, cons_label) => 
-		so ((
-		suppose
-		, args_match = def_fn .toString () .match (/\(((?:.|\s)*?)\)\s*=>/) [1] )=>
-		!! args_match ? so ((_=_=> 
-			faux_cons,
-			where
-			, faux_cons = (...vals) => (
+	) (
+	L .modify (L .values) ((def_fn, cons_label) => 
+		suppose (
+		( args_match = def_fn .toString () .match (/\(((?:.|\s)*?)\)\s*=>/) [1]
+		) =>
+		!! args_match
+		?
+			suppose (
+			( _cons = (...vals) => (
 					{ [cons_label]: R .fromPairs (R .zip (arg_labels, vals)) } )
 			, arg_labels = args_match .split (/\(.+?\)/g) .join ('') .split (',') .map (x => x .match (/([^\s=]+)\s*(?:=.+)?/) [1])
-			, $$1= __data_length .set (faux_cons, arg_labels .length)
-			, $$2= __data_lens .set (faux_cons, [cons_label]) )=>_)
-		: so ((_=_=> 
-			faux_cons,
-			where
-			, faux_cons = { [cons_label]: {} }
-			, $$1= __data_length .set (faux_cons, 0)
-			, $$2= __data_lens .set (faux_cons, [cons_label]) )=>_))))
+			, $__record_arglength = __data_length .set (_cons, arg_labels .length)
+			, $__record_lens = __data_lens .set (_cons, [cons_label])
+			) =>
+			_cons )
+		:
+			suppose (
+			( _cons = { [cons_label]: {} }
+			, $__record_arglength = __data_length .set (_cons, 0)
+			, $__record_lens = __data_lens .set (_cons, [cons_label])
+			) =>
+			_cons ) ) ))
 
 var cons_memoize = cons => 
 	memoize (cons
 	, { serializer: cons => {
-				if (! cons_memoize .ids .get (cons)) {
-					;var id = cons_memoize .next_id = (cons_memoize .next_id || 0) + 1
-					;cons_memoize .ids .set (cons, id) }
-				return cons_memoize .ids .get (cons) } })
-;cons_memoize .ids = new WeakMap
+		if (! cons_memoize .ids .get (cons)) {
+			;cons_memoize .next_id = (cons_memoize .next_id || 0) + 1
+			var id = cons_memoize .next_id
+			;cons_memoize .ids .set (cons, id) }
+		return cons_memoize .ids .get (cons) } })
+;cons_memoize .ids = new Map
 
 var data_lens = cons_memoize (cons =>
-	so ((_=_=>
-	faux_lens,
-	where
-	, faux_lens = __data_lens .get (cons)
-	, $$X=
-		!! not (Z .is (Z$ .AnyFunction) (cons)) ? 'nothing'
-		: so ((
-			suppose
-			, marked_template = so ((_=_=> 
+	suppose (
+	( _lens = __data_lens .get (cons)
+	, $__sub_lenses =
+		!! not (cons .constructor === Function) ? 'nothing'
+		: 
+			suppose (
+			( marked_template = so ((_=_=> 
 					T (cons) ([ apply, T (markers) ]),
 					where
 					, cons_length = __data_length .get (cons)
 					, markers = R .range (1) (cons_length + 1) )=>_)
-			, factors = T (marked_template) ([ R .values, sole, R .keys ]) )=>
+			, factors = T (marked_template) ([ R .values, sole, R .keys ])
+			) =>
 			T (factors) (R .forEach (_x => {
-				;faux_lens [_x] = [ faux_lens, _x ]})) ))=>_))
+				;_lens [_x] = [ _lens, _x ] })) )
+	) =>
+	_lens ) )
 
 var data_iso = cons_memoize (cons =>
-	so ((_=_=>
-	faux_lens,
-	where
-	, cons_fn = factors =>
-			!! not (Z .is (Z$ .AnyFunction) (cons)) ? cons
-			: T (cons) ([ apply, T (factors) ])
-	, marked_template = so ((_=_=> 
-			cons_fn (markers),
-			where
-			, cons_length = __data_length .get (cons)
-			, markers = R .range (1) (cons_length + 1) )=>_)
+	suppose (
+	( cons_fn = factors =>
+		!! not (cons .constructor === Function) ? cons
+		: T (cons) ([ apply, T (factors) ])
+	, marked_template = suppose (
+		( cons_length = __data_length .get (cons)
+		, markers = R .range (1) (cons_length + 1)
+		) =>
+		cons_fn (markers) )
 	, marked_factors = T (marked_template) ([ R .values, sole, R .invert ])
 	, ordered_factors = T (marked_factors) (L .collect ([ L .keyed, R .sortBy (L .get (L .first)), L .elems, L .last ]))
-	, record_to_factors = record => T (ordered_factors) (L .modify (L .elemsTotal) (_factor => record [_factor]))
+	, record_to_factors = record => T (ordered_factors) (L .modify (L .elemsTotal) (_factor => L .get (_factor) (record)))
 	, cons_label = sole (R .keys (marked_template))
-	, faux_lens = L .iso (L .get (cons_label)) ($ ([ record_to_factors, cons_fn ]))
-	, $$X=
+	, _iso = L .iso (L .get (cons_label)) ($ ([ record_to_factors, cons_fn ]))
+	, $__sub_lenses =
 		T (ordered_factors) (R .forEach (_x => {
-			;faux_lens [_x] = [ faux_lens, _x ] })) )=>_))
-/*
-var data_iso = data =>
-	where ((
-		read = where ((
-			instance_template = data .apply (null, R .range (1, data .__length + 1)),
-			inverted_template = R .invert (R .head (R .values (instance_template))),
-			ordered_factors = R .map (R .last) (R .sortBy (R .head) (R .toPairs (inverted_template))),
-			constructor_prefix = R .head (R .keys (instance_template)),
-			lenses = ordered_factors .map (_x => [constructor_prefix, _x])) =>
-			instance =>
-				lenses .map (lens => L .get (lens) (instance) )),
-		write = list =>
-			data .apply (null, list) ) =>
-		L .iso (read) (write))
-*/
+			;_iso [_x] = [ _iso, _x ] }))
+	) =>
+	_iso ) )
+
+
 var data_kind = by (data =>
 	L .get ([ L .keys, L .first ]))
 
@@ -178,49 +170,6 @@ var map_defined_ = default_ => fn => _x =>
 var map_defined = map_defined_ (undefined)
 var from_just = _x =>
 	Z_ .fromMaybe (undefined) (_x)
-/*var maybe_all_list = list =>
-	where ((
-		_head = Z_ .head (list),
-		_tail = Z_ .tail (list)) =>
-	T (_head) (Z_ .maybe (Z .Just ([]))
-		(maybe_head =>
-			where ((
-				maybe_tail = from_just (_tail) ) =>
-			T (maybe_head) (Z_ .chain (bare_head =>
-				T (maybe_all_list (maybe_tail)) (Z_ .chain (bare_tail =>
-					Z_ .Just (Z_ .prepend (bare_head) (bare_tail))))))))))
-var maybe_all = _x =>
-	!! Z .is (Z$ .Array (Z$ .Any)) (_x)
-	? maybe_all_list (_x)
-	: !! Z .is (Z$ .Object) (_x)
-	? T (_x) ([
-			R .toPairs,
-			Z_ .map (L .modify (L .first) (_x => Z_ .Just (_x))),
-			Z_ .map (maybe_all_list),
-			maybe_all_list,
-			Z_ .map (R .fromPairs) ])
-	: undefined
-*/
-//var maybe_all = Z_ .sequence (Z .Maybe)
-/* var to_maybe = so ((_=_=>
-	default_fn => _x => 
-		!! (Z_ .is (maybe_type_$)) (_x)
-		? _x
-		: default_fn (_x),
-	where
-	, maybe_type_$ = Z_ .MaybeType (Z$ .Any) )=>_) */
-
-
-
-
-
-
-/*
-var as_list = template =>
-	L .iso
-		(_x => template .map (lens => L .get (lens) (_x)))
-		(_x => ({ px: _x [0], py: _x [1], vx :_x [2], vy: _x [3] }))
-*/
 
 
 
@@ -235,21 +184,6 @@ var last_n = n => memoize (signal => S .root (_ => S (([ _, ...last_n_less_one ]
 
 
 
-/*var every = _x =>
-	so ((
-	suppose
-	, every = S .data (false)
-	, next = _ => {{
-			;every (true)
-			;setTimeout (next, _x) }}
-	, $$X = setTimeout (next, _x) ) =>
-	every)
-var delay = time => {{
-	var done = S .data (false)
-	;setTimeout (_ => {{
-		;done (true) }}
-	, time)
-	return done }}*/
 var shuffle = list => {
 	var array = []
 	for (var i in list) {
@@ -357,14 +291,10 @@ document .addEventListener ('DOMContentLoaded', _ => {
 	
 	;document .body .appendChild (view)
 	var dd = new (require ('diff-dom')) ({ valueDiffing: false })
-	//var morphdom = require ('morph-dom')
-	//var nanomorph = require ('nanomorph')
 	var morph = goals => {
 		;dd .apply (view, dd .diff (view, goals))
 		;[] .forEach .call (document .querySelectorAll ('input'), _input => {;_input .dispatchEvent (new Event ('input', { bubbles: true }))}) } 
 	
-		//;morphdom (view, goals) } 
-		//;nanomorph (view, goals) }
 	;morph (window .view), (new MutationObserver (mutations => {
 		;morph (window .view) }))
 	.observe (window .view,
